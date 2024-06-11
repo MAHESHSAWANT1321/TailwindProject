0cd53ee931c5c62dea39caa6b7515a95e461f953
MAHESHSAWANT1321 <sawantmahesh1321@gmail.com>
2024-06-11 09:50:24 +0530

    First Commit from Mahesh

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./dist/index.html"],//For specific file
+  content: ["./dist/*.html"],//For all file
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
